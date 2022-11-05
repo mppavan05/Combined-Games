02fdb396026e56afe7faebb2e2d35cd32dc940a0
mppavan05 <mppavan05@gmail.com>
2022-11-05 15:43:00 +0530

    ring game and snake game

---
+++ Combined/Assets/Scripts/Snake Scripts/Sphere.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Snake_Scripts
+{
+    public class Sphere : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (Manager.Instance.activeSnake == true)
+            {
+                //Add 1 node to snake
+                Manager.Instance.AddBodyPart();
+
+                //Add 1 to score
+                Manager.Instance.scoreCounter += 1;
+                Manager.Instance.speed += 0.025f;
+                Manager.Instance.SetCountText();
+
+                //Recycle sphere to a different position
+                Vector3 BallPosition = new Vector3(Random.Range(-8, 8), 0, Random.Range(-8, 8));
+                Manager.Instance.spawn_sphere(BallPosition);
+            }
+        }
+    }
+}
